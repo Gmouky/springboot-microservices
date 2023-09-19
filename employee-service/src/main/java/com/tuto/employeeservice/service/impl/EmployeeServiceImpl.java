ccaa807ead1bc2e45577ad992cca2d720e72d159
gmouky <gmouky@github.com>
2023-09-20 00:42:54 +0200

    adding webclient communication instead of restTemplate

--- employee-service/src/main/java/com/tuto/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/tuto/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -12,6 +12,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,7 +23,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
     private ModelMapper modelMapper;
-    private RestTemplate restTemplate;
+    //private RestTemplate restTemplate;
+
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -43,11 +46,17 @@ public class EmployeeServiceImpl implements EmployeeService {
     public APIResponseDto getEmployee(Long employeeId) {
         Employee employee = getEmployeeById(employeeId);
 
-        ResponseEntity<DepartmentDto> responseEntity =
+        /*ResponseEntity<DepartmentDto> responseEntity =
                 restTemplate.getForEntity("http://localhost:8080/api/departments/code/" + employee.getDepartmentCode(),
-                DepartmentDto.class);
+                DepartmentDto.class);*/
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+        //DepartmentDto departmentDto = responseEntity.getBody();
+
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/code/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setEmployeeDto(modelMapper.map(employee, EmployeeDto.class));
