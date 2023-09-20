b1141ac394982096af5219f374bad5e471773b82
gmouky <gmouky@github.com>
2023-09-20 14:05:38 +0200

    adding service registry and configure eureka client in other services

---
+++ service-registry/src/test/java/com/tuto/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuto.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
